604b10681ea7ae68b237a36d0fa085f84e4f4662
Mahendra Telure <teluremahendra@gmail.com>
2024-02-18 23:10:18 +0530

    Assignment question 2 Done
    2.	Write a program to count the total number of digits in a number

---
+++ Python Assignments/2 Write a program to count the total number of digits in a number.py
@@ -0,0 +1,8 @@
+# 2.	Write a program to count the total number of digits in a number
+num = int(input("Enter a number : "))
+cnt=0
+while(num>0):
+    num = num // 10
+    cnt += 1
+    
+print("Total digits : {0}".format(cnt))
