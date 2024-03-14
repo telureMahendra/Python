932b72c6f8e61771077d5d90e2dce46812ce26e6
Mahendra Telure <teluremahendra@gmail.com>
2024-03-14 11:54:49 +0530

    Practical No.: 12.
    Write a program to demonstrate Calling function from another file

---
+++ Python Practical/12 calling function from another file/Student.py
@@ -0,0 +1,8 @@
+""" Practical No.: 12. Write a program to demonstrate Calling function from another file """
+from functions import *
+
+sname = readName()
+marks = readMarks()
+
+print("Name is : ", sname)
+print("Marks is : ", marks)
---
+++ Python Practical/12 calling function from another file/functions.py
@@ -0,0 +1,6 @@
+# file having functions
+
+def readMarks():
+    return (int(input("Enter a Marks : ")))
+def readName():
+    return (input("Enter a Student name : "))
