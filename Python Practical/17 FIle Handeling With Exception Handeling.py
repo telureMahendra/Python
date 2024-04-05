b2ca816988b8bde9749e01b1e3caf0392a8fc9ab
Mahendra Telure <teluremahendra@gmail.com>
2024-04-05 12:28:46 +0530

    Practical 17.1

---
+++ Python Practical/17 FIle Handeling With Exception Handeling.py
@@ -0,0 +1,34 @@
+"""
+17: Create a list 10 elements. 
+    Write a program to write this list in binary file and then 
+    read it back to find out the smallest and largest value 
+"""
+def findSL(li):
+    sm = li[0]
+    lg = li[0]
+    for i in li:
+        if sm > i:
+            sm = i
+        if lg < i:
+            lg = i
+    print("Smallest Element is : ", sm)
+    print("Largest Element is : ", lg)
+            
+
+li = [2,8,1,9,5,7,4,6,3,10]
+
+fp = open("listFile.bin", "wb" )
+# fp.write(bytes(li))
+fp.write(bytearray(li))
+fp.close()
+
+try:
+    fp1 = open("listFile.bin", "rb" )
+    data = list(fp1.read())
+except FileNotFoundError:
+    print("File Does Not Exists...") 
+
+
+print("Smallest Element is : ", min(data))
+print("Largest Element is : ", max(data))
+# findSL(data)
